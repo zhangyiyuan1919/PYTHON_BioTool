f1ce76ec26f4a340c31933ace6e4c9cbf6ccb523
zhangyiyuan1919 <zhangyiyuan1919@gmail.com>
2026-04-14 10:18:34 +0800

    Add files via upload

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,10 +1,8 @@
 #DNA工具包
-
 import collections
 from structures import *
 
-# 工具1：定义合法DNA碱基（支持IUPAC简并碱基可自行扩展）
-def validateSeq(dna_seq):
+def validateSeq(dna_seq):# 工具1：定义合法DNA碱基（支持IUPAC简并碱基可自行扩展）
     """
     严格校验DNA序列：
     - 若包含任何非A/T/C/G的字符，直接抛出ValueError，明确提示非法字符
@@ -19,8 +17,7 @@ def validateSeq(dna_seq):
         raise ValueError(f"❌ DNA序列包含非法字符: {sorted(invalid_chars)}")
     return tmpseq
 
-#工具2：统计碱基数量
-def countFrequency(seq):
+def countFrequency(seq):#工具2：统计碱基数量
     tmpFreqDict={"A":0,"T":0,"C":0,"G":0} #计数字典
     for nuc in seq:
         tmpFreqDict[nuc]+=1
@@ -28,13 +25,26 @@ def countFrequency(seq):
 #另一种解法
 #def countNucFrequency(seq):
 #    return dict(collections.Counter(seq))
-
-#工具3：序列转录 DNA→RNA
-def transcription(seq):
+def transcription(seq):#工具3：序列转录 DNA→RNA
     """DNA→RNA转录，用U代替T"""  #在自定工具包中相当于加入注释
     return seq.replace("T","U")
 
-#工具4：互补序列
-def reverse_complement(seq):
+def reverse_complement(seq):#工具4：互补序列
     """DNA的互补链，顺序是5‘→3’"""
-    return ''.join([DNA_ReverseComplement[Nuc] for Nuc in seq])[::-1]#[::-1]首位调转互补后的序列
+   # return ''.join([DNA_ReverseComplement[Nuc] for Nuc in seq])[::-1]#[::-1]首位调转互补后的序列
+    # PY方法更快捷一些
+    mapping=str.maketrans('ATCG','TAGC')
+    return seq.translate(mapping)[::-1]
+
+
+#工具5：基因密度统计➡️gc含量统计
+def gc_content(seq):
+    """在一段DNA或者RNA上GC含量"""
+    return round((seq.count('C')+seq.count('G'))/len(seq)*100)
+#滑动窗口 GC 含量统计
+def gc_content_subsec(seq,k=20):
+    res=[]
+    for i in range(0,len(seq)-k+1,k):
+        subseq=seq[i:i+k]
+        res.append(gc_content(subseq))
+    return res
